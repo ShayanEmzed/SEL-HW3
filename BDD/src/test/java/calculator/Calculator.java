b056790c42eec8e502b8de0aaa39a9fca2f2c043
ShayanEmzed <shayan80sama@gmail.com>
2023-08-05 01:17:34 +0330

    Change calculator class to pass the scenario

--- BDD/src/test/java/calculator/Calculator.java
+++ BDD/src/test/java/calculator/Calculator.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.lang.Math;
+
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+
+    public int doOperation(int a, int b, char opt) {
+        if (opt == '*') {
+            return a * b;
+        } else if (opt == '/') {
+            return a / b;
+        } else if (opt == '^') {
+            return (int) Math.pow(a, b);
+        }
+        return 0;
     }
 }
